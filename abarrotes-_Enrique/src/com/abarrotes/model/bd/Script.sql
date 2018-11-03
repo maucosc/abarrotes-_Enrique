1f1cb1908e36ea83ab9ed2efc583a76f31acdbe8
marco silva cadiz <mauco.sc@outlook.es>
2018-11-03 00:07:22 -0300

    Creacion de categorias de bases de datos

--- abarrotes-_Enrique/src/com/abarrotes/model/bd/Script.sql
+++ abarrotes-_Enrique/src/com/abarrotes/model/bd/Script.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE bd_abarrotes;
-USE bdCREATE DATABASE bd_abarrotes;
 USE bd_abarrotes;
 
 CREATE TABLE marca(
@@ -14,17 +13,6 @@ INSERT INTO marca VALUES (NULL, 'Soprole');
 INSERT INTO marca VALUES (NULL, 'Marco Polo');
 INSERT INTO marca VALUES (NULL, 'Danone');
 
-CREATE TABLE pais(
-	id INT AUTO_INCREMENT,
-	nombre VARCHAR (100),
-	
-	PRIMARY KEY (id),
-	UNIQUE (nombre)
-);
-
-INSERT INTO pais VALUES (NULL, 'Chile');
-INSERT INTO pais VALUES (NULL, 'Peru');
-INSERT INTO pais VALUES (NULL, 'Argentina');
 
 CREATE TABLE vendedor(
 	id INT AUTO_INCREMENT,
@@ -37,13 +25,31 @@ CREATE TABLE vendedor(
 INSERT INTO vendedor VALUES (NULL, 'Juan Carlos');
 INSERT INTO vendedor VALUES (NULL, 'Camila Soto');
 
+CREATE TABLE categoria (
+	id INT AUTO_INCREMENT,
+	nombre VARCHAR (100),
+	
+	PRIMARY KEY (id),
+	UNIQUE (nombre)
+);
+
+INSERT INTO categoria VALUES (NULL, 'Productos enlatados')
+INSERT INTO categoria VALUES (NULL, 'Lacteos')
+INSERT INTO categoria VALUES (NULL, 'Frutas y verduras')
+INSERT INTO categoria VALUES (NULL, 'Bebidas')
+INSERT INTO categoria VALUES (NULL, 'Bebidas Alcohòlicas')
+INSERT INTO categoria VALUES (NULL, 'Alimentos preparados')
+INSERT INTO categoria VALUES (NULL, 'Automedicacion')
+INSERT INTO categoria VALUES (NULL, 'Higiene Personal')
+INSERT INTO categoria VALUES (NULL, 'Uso domesticos')
+INSERT INTO categoria VALUES (NULL, '')
+
 CREATE TABLE proveedor(
 	id INT AUTO_INCREMENT,
 	nombre VARCHAR (100),
 	telefono VARCHAR (20),
 	direccion VARCHAR (200),
 	email VARCHAR (20),
-	fk_pais INT,
 	
 	PRIMARY KEY (id)
 );
@@ -51,23 +57,41 @@ CREATE TABLE proveedor(
 INSERT INTO proveedor VALUES (NULL, 'cocacola', '900199202', 'Circunvalación Nte 41, Rancagua, Región del Libertador Gral. Bernardo O’Higgins', '', 1);
 INSERT INTO proveedor VALUES (NULL, 'Soprole', '800220011', 'Avenida Las Torres #90, Bodega D , Rancagua', 'soprole@soprole.cl', 1);
 
+CREATE TABLE estado_cliente (
+	id INT AUTO_INCREMENT,
+	nombre VARCHAR (100),
+	total_deuda VARCHAR (100)
+);
+
 
 CREATE TABLE producto(
 	id INT AUTO_INCREMENT,
 	nombre VARCHAR (100),
 	stock INT,
 	precio INT,
-	fk_producto INT,
+	fk_proveedor INT,
 	fk_marca INT,
 	
 	PRIMARY KEY (id)
-	FOREIGN KEY (fk_producto) REFERENCES producto(id),
+	FOREIGN KEY (fk_proveedor) REFERENCES proveedor(id),
 	FOREIGN KEY (fk_marca) REFERENCES marca(id)
+);
+
+CREATE TABLE venta (
+	id INT AUTO_INCREMENT,
+	fecha DATE,
+	total INT,
+	fk_vendedor INT,
+	
+	PRIMARY KEY (id),
+	FOREIGN KEY (fk_vendedor) REFERENCES vendedor(id)
+	
 );
 
 /*
 Continuar con sql
 */
+
 
 
 
